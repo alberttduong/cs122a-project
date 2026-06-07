801a65be67e0e72d11e5eb3cf67a2cd2688112da
datn93788@gmail.com <dattn10@uci.edu>
2026-06-07 21:55:46 +0000

    Feat : Implemented organizer_stats, participant_schedule, and popular_event_types

--- organizer_stats.py
+++ organizer_stats.py
@@ -1,4 +1,29 @@
 import sys
-
+ 
 def organizer_stats(mydb):
-    pass
+    n = sys.argv[2]
+ 
+    mycursor = mydb.cursor()
+ 
+    try:
+        mycursor.execute(
+            """
+            SELECT o.uid, u.username, o.department, COUNT(e.eid) AS eventCount
+            FROM Organizer o
+            JOIN User u ON o.uid = u.uid
+            JOIN Event e ON e.creator_uid = o.uid
+            GROUP BY o.uid, u.username, o.department
+            HAVING eventCount >= %s
+            ORDER BY eventCount DESC, o.uid ASC
+            """,
+            (n,)
+        )
+        rows = mycursor.fetchall()
+        for row in rows:
+            print(",".join(str(col) for col in row))
+    except Exception as e:
+        mydb.rollback()
+        print("Fail")
+    finally:
+        mycursor.close()
+ 
--- popular_event_types.py
+++ popular_event_types.py
@@ -1,4 +1,29 @@
 import sys
-
+ 
 def popular_event_types(mydb):
-    pass
+    n = sys.argv[2]
+ 
+    mycursor = mydb.cursor()
+ 
+    try:
+        mycursor.execute(
+            """
+            SELECT e.type, COUNT(s.snum) AS reservedCount
+            FROM Event e
+            JOIN Slot s ON e.eid = s.eid
+            WHERE s.is_reserved = TRUE
+            GROUP BY e.type
+            HAVING reservedCount >= %s
+            ORDER BY reservedCount DESC, e.type ASC
+            """,
+            (n,)
+        )
+        rows = mycursor.fetchall()
+        for row in rows:
+            print(",".join(str(col) for col in row))
+    except Exception as e:
+        mydb.rollback()
+        print("Fail")
+    finally:
+        mycursor.close()
+ 
