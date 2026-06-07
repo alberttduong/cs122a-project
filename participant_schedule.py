07a3aa6fc5f88f9b1f1f0719895e1415ccf8dfa1
Sriram Reddy Kudamala <skudamal@uci.edu>
2026-06-07 15:47:52 -0700

    fixed participant_schedule

--- participant_schedule.py
+++ participant_schedule.py
@@ -1,30 +1,42 @@
 import sys
- 
+from datetime import datetime, date
+
 def participant_schedule(mydb):
     uid = sys.argv[2]
- 
+
     mycursor = mydb.cursor()
- 
+
     try:
         mycursor.execute(
             """
             SELECT e.eid, e.title, e.type, e.datetime, s.snum,
-                   v.vid, v.street, v.city, v.state, v.zip
+                   h.vid, v.street, v.city, v.state, v.zip
             FROM Slot s
             JOIN Event e ON s.eid = e.eid
-            LEFT JOIN Hosting h ON e.eid = h.eid AND h.is_primary = TRUE
+            LEFT JOIN (
+                SELECT eid, vid FROM Hosting WHERE is_primary = TRUE
+            ) h ON e.eid = h.eid
             LEFT JOIN Venue v ON h.vid = v.vid
-            WHERE s.uid = %s
+            WHERE s.uid = %s AND s.is_reserved = TRUE
             ORDER BY e.datetime ASC
             """,
             (uid,)
         )
         rows = mycursor.fetchall()
         for row in rows:
-            print(",".join("" if col is None else str(col) for col in row))
+            formatted = []
+            for col in row:
+                if col is None:
+                    formatted.append("")
+                elif isinstance(col, datetime):
+                    formatted.append(col.strftime("%Y-%m-%d %H:%M:%S"))
+                elif isinstance(col, date):
+                    formatted.append(col.strftime("%Y-%m-%d"))
+                else:
+                    formatted.append(str(col))
+            print(",".join(formatted))
     except Exception as e:
         mydb.rollback()
         print("Fail")
     finally:
         mycursor.close()
- 
