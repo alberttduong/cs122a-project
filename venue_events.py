4aabd4e534a972b7d110309f4d371652c5d9794f
Sriram Reddy Kudamala <skudamal@uci.edu>
2026-06-07 15:36:17 -0700

    implemented venue_events

--- venue_events.py
+++ venue_events.py
@@ -1,4 +1,25 @@
 import sys
 
 def venue_events(mydb):
-    pass
+    vid = sys.argv[2]
+
+    mycursor = mydb.cursor()
+
+    try:
+        mycursor.execute(
+            """
+            SELECT e.eid, e.title, e.type, e.datetime, h.is_primary
+            FROM Event e
+            JOIN Hosting h ON e.eid = h.eid
+            WHERE h.vid = %s
+            ORDER BY e.datetime ASC, e.eid ASC
+            """,
+            (vid,)
+        )
+        rows = mycursor.fetchall()
+        for row in rows:
+            print(",".join(str(col) for col in row))
+    except Exception as e:
+        print("Fail")
+    finally:
+        mycursor.close()
