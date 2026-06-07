2cd5da0e98b687c3cb7cfd5b7d80f2e0276ccddc
Sriram Reddy Kudamala <skudamal@uci.edu>
2026-06-07 15:32:37 -0700

    implemented delete_organizer and available_events

--- available_events.py
+++ available_events.py
@@ -1,4 +1,27 @@
 import sys
 
 def available_events(mydb):
-    pass
+    date = sys.argv[2]
+
+    mycursor = mydb.cursor()
+
+    try:
+        mycursor.execute(
+            """
+            SELECT e.eid, e.title, e.type, e.datetime, COUNT(s.snum) AS availableSlots
+            FROM Event e
+            JOIN Slot s ON e.eid = s.eid
+            WHERE s.is_reserved = FALSE
+            AND e.datetime > %s
+            GROUP BY e.eid, e.title, e.type, e.datetime
+            ORDER BY e.datetime ASC, e.eid ASC
+            """,
+            (date,)
+        )
+        rows = mycursor.fetchall()
+        for row in rows:
+            print(",".join(str(col) for col in row))
+    except Exception as e:
+        print("Fail")
+    finally:
+        mycursor.close()
